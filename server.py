48600126e44b61d52f12d4ff83138a3eeec4ad87
John J Czaplewski <jczaplew@gmail.com>
2016-07-26 12:19:32 -0500

    Validate post params

--- server.py
+++ server.py
@@ -7,13 +7,13 @@ app = Flask(__name__)
 @app.route('/', methods=['GET', 'POST'])
 def default():
     if request.method == 'POST':
-        print type(request.form['shape'])
-        print ast.literal_eval(request.form['shape'])
+        if 'shape' not in request.form or 'age' not in request.form:
+            return jsonify(**{'error': 'A shape and age are required. See documentation at https://github.com/UW-Macrostrat/gplates-reconstruct'})
         reconstructed = reconstruct(ast.literal_eval(request.form['shape']), request.form.get('age', type=int))
         return jsonify(**reconstructed)
     else:
         if not request.args.get('lat') or not request.args.get('lng') or not request.args.get('age'):
-            return jsonify(**{'error': 'A lng, lat, and age are required'})
+            return jsonify(**{'error': 'A lng, lat, and age are required. See documentation at https://github.com/UW-Macrostrat/gplates-reconstruct'})
 
         reconstructed = reconstruct({'type': 'Point', 'coordinates': [float(request.args.get('lng')), float(request.args.get('lat'))]}, int(request.args.get('age')))
 
