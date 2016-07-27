790fdc81138204abe54e38974e7840e4417f712d
Michael McClennen <mmcclenn@geology.wisc.edu>
2016-07-27 16:32:38 -0500

    Adding a new argument 'points', which allows an alternate request syntax. This will probably have some syntax errors and will need to be fixed in a subsequent commit.
    Also added quotes around URLs in 'setup.sh'.

--- server.py
+++ server.py
@@ -11,6 +11,19 @@ def default():
             return jsonify(**{'error': 'A shape and age are required. See documentation at https://github.com/UW-Macrostrat/gplates-reconstruct'})
         reconstructed = reconstruct(ast.literal_eval(request.form['shape']), request.form.get('age', type=int))
         return jsonify(**reconstructed)
+    elif request.args.get('points'):
+        if not request.args.get('age'):
+            return jsonify(**{'error': 'You must specify an age'})
+        points = request.args.get('points').split()
+        age = int(request.args.get('age'))
+        result = [ ]
+        for p in points:
+            coords = p.split(',')
+            reconstructed = reconstruct({'type': 'Point', 'coordinates': [float(coords[0]), float(coords[1])]}, age)
+            if coords.len > 2:
+                reconstructed.selector = coords[2]
+            result.append(reconstructed)
+        return jsonify(**{'result': result})
     else:
         if not request.args.get('lat') or not request.args.get('lng') or not request.args.get('age'):
             return jsonify(**{'error': 'A lng, lat, and age are required. See documentation at https://github.com/UW-Macrostrat/gplates-reconstruct'})
--- setup.sh
+++ setup.sh
@@ -1,24 +1,24 @@
-curl -L -o pygplates.zip http://downloads.sourceforge.net/project/gplates/pygplates/beta-revision-12/pygplates_rev12_python27_MacOS64.zip?r=&ts=1467312228&use_mirror=tenet
+curl -L -o pygplates.zip 'http://downloads.sourceforge.net/project/gplates/pygplates/beta-revision-12/pygplates_rev12_python27_MacOS64.zip?r=&ts=1467312228&use_mirror=tenet'
 unzip -a pygplates.zip
 rm pygplates.zip
 
-curl -L -O ftp://earthbyte.org/earthbyte/GPlates/SampleData_GPlates1.5/Individual/FeatureCollections/Rotations.zip
+curl -L -O 'ftp://earthbyte.org/earthbyte/GPlates/SampleData_GPlates1.5/Individual/FeatureCollections/Rotations.zip'
 unzip -a Rotations.zip
 rm Rotations.zip
 
-curl -L -O ftp://earthbyte.org/earthbyte/GPlates/SampleData_GPlates1.5/Individual/FeatureCollections/ContinentalPolygons.zip
+curl -L -O 'ftp://earthbyte.org/earthbyte/GPlates/SampleData_GPlates1.5/Individual/FeatureCollections/ContinentalPolygons.zip'
 unzip -a ContinentalPolygons.zip
 rm ContinentalPolygons.zip
 
-curl -L -O ftp://ftp.earthbyte.org/papers/Wright_etal_Paleobiogeography/1_Phanerozoic_Plate_Motions_GPlates.zip
+curl -L -O 'ftp://ftp.earthbyte.org/papers/Wright_etal_Paleobiogeography/1_Phanerozoic_Plate_Motions_GPlates.zip'
 unzip -a 1_Phanerozoic_Plate_Motions_GPlates.zip
 rm 1_Phanerozoic_Plate_Motions_GPlates.zip
 
-curl -L -O ftp://earthbyte.org/earthbyte/GPlates/SampleData_GPlates1.5/Individual/FeatureCollections/StaticPolygons.zip
+curl -L -O 'ftp://earthbyte.org/earthbyte/GPlates/SampleData_GPlates1.5/Individual/FeatureCollections/StaticPolygons.zip'
 unzip -a StaticPolygons.zip
 rm StaticPolygons.zip
 
-curl -L -O http://download.osgeo.org/libspatialindex/spatialindex-src-1.8.5.tar.gz
+curl -L -O 'http://download.osgeo.org/libspatialindex/spatialindex-src-1.8.5.tar.gz'
 tar -zxvf spatialindex-src-1.8.5.tar.gz
 rm spatialindex-src-1.8.5.tar.gz
 cd spatialindex-src-1.8.5
